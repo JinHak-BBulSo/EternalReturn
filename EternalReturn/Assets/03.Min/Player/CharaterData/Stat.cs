849b260d8fdf863d99fbfdd2a4e131ddd3c8a758
qkqakseb <parknanju6376@gmail.com>
2023-04-12 17:41:26 +0900

    [Mingu] Player Move

---
+++ EternalReturn/Assets/03.Min/Player/CharaterData/Stat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    public float attackPower;
+    public float skillPower;
+    public float basicAttackPower;
+    public float defense;
+    public float attackSpeed;
+    public float coolDown;
+    public float criticalPercent;
+    public float criticalDamage;
+    public float moveSpeed;
+    public float visionRange;
+    public float attackRange;
+    public float damageReduce;
+    public float tenacity;
+    public float armorReduce;
+    public float lifeSteel;
+    public float extraHp;
+    public float extraStamina;
+    public float hpRegen;
+    public float staminaRegen;
+}
