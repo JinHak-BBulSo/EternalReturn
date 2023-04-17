17786a38bb49b34271b9c195efcde67029e6d931
EricK-00 <carter000@naver.com>
2023-04-17 17:44:28 +0900

    [TaeHee] fow test

---
+++ EternalReturn/Assets/01.TaeHee/Test/Scripts/PreRenderCalls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PreRenderCalls : MonoBehaviour
+{
+    public bool UsingURP;
+    public Fog _Fog;
+
+    void Awake()
+    {
+        if (UsingURP)
+        {
+            RenderPipelineManager.beginCameraRendering += PreRenderURP;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (UsingURP)
+        {
+            RenderPipelineManager.beginCameraRendering -= PreRenderURP;
+        }
+    }
+
+    void PreRenderURP(ScriptableRenderContext renderContext, Camera obj)
+    {
+        Debug.Log("PreRender");
+        OnPreRender();
+    }
+
+    void OnPreRender()
+    {
+        Debug.Log("Set");
+        // FOG CALL
+        _Fog.SetCookie();
+    }
+}
