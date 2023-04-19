29be77c2f2cbd8fafdce99683c3f76f6dd0825a0
BBulSo <kjhbbulso@naver.com>
2023-04-19 21:34:47 +0900

    [KJH] URP to Built-in Set

--- EternalReturn/Assets/00.JinHak/Script/Monster/MonsterBattleArea.cs
+++ EternalReturn/Assets/00.JinHak/Script/Monster/MonsterBattleArea.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
-
 public class MonsterBattleArea : MonoBehaviour
 {
     public Monster monster = default;
