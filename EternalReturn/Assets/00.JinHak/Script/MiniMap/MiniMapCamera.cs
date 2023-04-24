378944b54433443c6e55169aaec94470dbfb40fe
BBulSo <kjhbbulso@naver.com>
2023-04-24 23:00:41 +0900

    [KJH] ItemBox Prefab Add

--- EternalReturn/Assets/00.JinHak/Script/MiniMap/MiniMapCamera.cs
+++ EternalReturn/Assets/00.JinHak/Script/MiniMap/MiniMapCamera.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+using UnityEngine.AI;
 
 public class MiniMapCamera : MonoBehaviour
 {
+    private Camera miniMapCamera = default;
 
+    private void Start()
+    {
+        miniMapCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(miniMapCamera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Camera.main.transform.position = hit.point;
+            }
+        }
+    }
 }
