5688e037b0d70b3caa66631a66ca00f0bc3c50d0
BBulSo <108716571+BBulSo@users.noreply.github.com>
2023-04-27 15:26:13 +0900

    [KJH] Skill mesh update

--- EternalReturn/Assets/00.JinHak/Script/Monster/Monster/MonsterBoar.cs
+++ EternalReturn/Assets/00.JinHak/Script/Monster/Monster/MonsterBoar.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boar : MonoBehaviour
+public class MonsterBoar : Monster
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void Skill()
     {
-        
-    }
+        base.Skill();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
